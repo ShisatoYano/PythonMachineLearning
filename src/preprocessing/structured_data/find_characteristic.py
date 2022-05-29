dbc5d984c213e597272e6287c9f4375e67186388
Shisato <shisatoyano@gmail.com>
2022-05-29 14:07:53 +0000

    add min-max normalization sample

---
+++ src/preprocessing/structured_data/find_characteristic.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+import tkinter.filedialog as tkfd
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+
+root = tk.Tk()
+root.withdraw()
+
+# select csv file
+csv_path = tkfd.askopenfilename(filetypes=[("CSV", "*.csv")])
+bank_df = pd.read_csv(csv_path, sep=',')
+print(bank_df.shape)
+print(bank_df.dtypes)
+
+# normalize data
+# min-max normalization
+bank_df = bank_df.drop("y", axis=1)
+mc = MinMaxScaler()
+mc.fit(bank_df)
+bank_df_mc = pd.DataFrame(mc.transform(bank_df), columns=bank_df.columns)
+print(bank_df_mc)
