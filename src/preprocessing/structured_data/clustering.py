263b784eeab64fe4a1e7329d7212291acb1e992c
Shisato <shisatoyano@gmail.com>
2022-05-31 13:52:43 +0000

    add clustering.py

---
+++ src/preprocessing/structured_data/clustering.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+import tkinter.filedialog as tkfd
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from scipy.cluster.hierarchy import linkage, dendrogram
+import matplotlib.pyplot as plt
+
+root = tk.Tk()
+root.withdraw()
+
+# select csv file
+csv_path = tkfd.askopenfilename(filetypes=[("CSV", "*.csv")])
+bank_df = pd.read_csv(csv_path, sep=',')
+
+# normalize data
+# z-score normalization
+sc = StandardScaler()
+sc.fit(bank_df)
+bank_df_sc = pd.DataFrame(sc.transform(bank_df), columns=bank_df.columns)
+
+# hierarchical clustering by ward's method
+hcls = linkage(bank_df_sc, metric="euclidean", method="ward")
+dendrogram(hcls)
+
+plt.show()
