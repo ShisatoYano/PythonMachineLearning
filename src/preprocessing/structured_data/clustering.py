0bf659d6bb28f93903e4d026e24d3df7700c7fa7
Shisato <shisatoyano@gmail.com>
2022-05-31 14:12:13 +0000

    add ward method sample

--- src/preprocessing/structured_data/clustering.py
+++ src/preprocessing/structured_data/clustering.py
@@ -2,7 +2,7 @@ import tkinter as tk
 import tkinter.filedialog as tkfd
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
-from scipy.cluster.hierarchy import linkage, dendrogram
+from scipy.cluster.hierarchy import linkage, dendrogram, fcluster
 import matplotlib.pyplot as plt
 
 root = tk.Tk()
@@ -21,5 +21,11 @@ bank_df_sc = pd.DataFrame(sc.transform(bank_df), columns=bank_df.columns)
 # hierarchical clustering by ward's method
 hcls = linkage(bank_df_sc, metric="euclidean", method="ward")
 dendrogram(hcls)
+# cluster id
+cst_group = fcluster(hcls, 100, criterion="distance")
+print(cst_group)
+
+# non-hierarchical clustering by k-means method
+
 
 plt.show()
