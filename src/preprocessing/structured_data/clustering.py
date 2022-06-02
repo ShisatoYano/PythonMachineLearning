8e7812b1048e091ee0dcdb3ba7e2de7386134adf
Shisato <shisatoyano@gmail.com>
2022-06-02 13:15:59 +0000

    add kmeans sample

--- src/preprocessing/structured_data/clustering.py
+++ src/preprocessing/structured_data/clustering.py
@@ -2,6 +2,7 @@ import tkinter as tk
 import tkinter.filedialog as tkfd
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
 from scipy.cluster.hierarchy import linkage, dendrogram, fcluster
 import matplotlib.pyplot as plt
 
@@ -20,12 +21,20 @@ bank_df_sc = pd.DataFrame(sc.transform(bank_df), columns=bank_df.columns)
 
 # hierarchical clustering by ward's method
 hcls = linkage(bank_df_sc, metric="euclidean", method="ward")
-dendrogram(hcls)
+# dendrogram(hcls)
 # cluster id
 cst_group = fcluster(hcls, 100, criterion="distance")
 print(cst_group)
 
 # non-hierarchical clustering by k-means method
-
+kcls = KMeans(n_clusters=10)
+cst_group_k = kcls.fit_predict(bank_df_sc)
+print(cst_group_k)
+for i in range(10):
+    labels = bank_df_sc[cst_group_k == i]
+    plt.scatter(labels["age"], labels["balance"], label=i)
+plt.legend()
+plt.xlabel("age")
+plt.ylabel("balance")
 
 plt.show()
