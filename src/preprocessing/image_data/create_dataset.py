538252aaabbb14753c0ebf208f76824857975697
Shisato <shisatoyano@gmail.com>
2022-08-29 08:19:52 +0000

    create pixels, labels dataframe

---
+++ src/preprocessing/image_data/create_dataset.py
@@ -0,0 +1,41 @@
+import tkinter as tk
+import tkinter.filedialog as tkfd
+import os
+import cv2
+import numpy as np
+import pandas as pd
+
+root = tk.Tk()
+root.withdraw()
+
+# define data set array
+pixels = []
+labels = []
+
+# select directory
+selected_dir = tkfd.askdirectory()
+
+# append pixel value and label of each image to data set array
+for current_dir, sub_dirs, file_list in os.walk(selected_dir):
+    if len(sub_dirs) > 0:
+        for i, d in enumerate(sub_dirs):
+            sub_dir_path = current_dir + '/' + d
+            files = os.listdir(sub_dir_path)
+            
+            for f in files:
+                img = cv2.imread(sub_dir_path + '/' + f, 0)
+                img = cv2.resize(img, (128, 128))
+                img = np.array(img).flatten().tolist()  # convert 2d array to 1d array
+                pixels.append(img)
+
+                labels.append(i)
+
+# convert data set from list to dataframe
+pixels_df = pd.DataFrame(pixels)
+pixels_df = pixels_df / 255  # normalize pixel between 0 and 1
+
+labels_df = pd.DataFrame(labels)
+labels_df = labels_df.rename(columns={0: "label"})
+
+img_set = pd.concat([pixels_df, labels_df], axis=1)
+print(img_set.head())
