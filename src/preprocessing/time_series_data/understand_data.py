0b3d03887d806e7a448514c1ecc0045575d22349
Shisato <shisatoyano@gmail.com>
2022-09-04 08:37:50 +0000

    implemented find and complement nan

--- src/preprocessing/time_series_data/understand_data.py
+++ src/preprocessing/time_series_data/understand_data.py
@@ -2,3 +2,28 @@ import pandas as pd
 
 data_df = pd.read_csv("/workspaces/PythonMachineLearning/preprocess_sample_data/chap6/data/data/energydata.csv")
 print(data_df.head())
+
+# data's shape
+print(data_df.shape)
+
+# data's type
+print(data_df.dtypes)
+
+# convert to datetime
+data_df["date"] = pd.to_datetime(data_df["date"], format="%Y-%m-%d %H:%M:%S")
+print(data_df["date"].dtypes)
+print(type(data_df["date"][0]))
+
+# calculate epapsed time from initial time
+data_df["dif_min"] = data_df["date"].diff().dt.total_seconds() / 60
+data_df["dif_min"] = data_df["dif_min"].fillna(0)
+print(data_df["dif_min"].head())
+data_df["cum_min"] = data_df["dif_min"].cumsum()
+print(data_df[["date", "cum_min"]].head())
+
+# calculate statistics
+print(data_df.describe())
+
+# find and complement Nan
+print(data_df.isnull().sum(axis=1).sort_values(ascending=False))
+print(data_df.isnull().sum(axis=0))
