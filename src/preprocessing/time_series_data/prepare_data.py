4033acafade2d2f124ea672dccf6814328e2e375
Shisato <shisatoyano@gmail.com>
2022-09-07 15:01:38 +0000

    calculate mean by 6 hours

--- src/preprocessing/time_series_data/prepare_data.py
+++ src/preprocessing/time_series_data/prepare_data.py
@@ -11,3 +11,11 @@ data_df["dif_min"] = data_df["date"].diff().dt.total_seconds() / 60
 data_df["dif_min"] = data_df["dif_min"].fillna(0)
 data_df["cum_min"] = data_df["dif_min"].cumsum()
 print(data_df[["date", "cum_min"]])
+
+# mean, std per 6 hours
+data_df["cum_6hour"] = (data_df["cum_min"] / 360).round(2).astype(int)
+print(data_df["cum_6hour"].unique())
+print(data_df[["date", "cum_min", "cum_6hour"]].head(50))
+data_df = data_df.drop(["date", "dif_min", "cum_min"], axis=1)
+data_df_mean = data_df.groupby("cum_6hour").mean()
+print(data_df_mean.head())
