7fc1ad2f96740afef820e8392ad9f4c625c6f3bd
Shisato <shisatoyano@gmail.com>
2022-09-18 14:20:14 +0000

    add sample analyze by janome

---
+++ src/preprocessing/natural_language_data/understand_data.py
@@ -0,0 +1,25 @@
+from janome.tokenizer import Tokenizer
+from janome.analyzer import Analyzer
+from janome.tokenfilter import POSKeepFilter
+import re
+
+
+# morphological analysis sample by Janome
+f = open("/workspaces/PythonMachineLearning/preprocess_sample_data/chap7/data/data/it-life-hack/it-life-hack-6292880.txt", encoding="utf-8")
+txt = f.read()
+print(txt)
+
+# regular expression
+reg_txt = re.sub(r"[0-9a-zA-Z]+", '', txt)
+reg_txt = re.sub(r"[:;/+\.~]", '', reg_txt)
+reg_txt = re.sub(r"[\s\n]", '', reg_txt)
+print(reg_txt)
+
+# analyze
+t = Tokenizer()
+token_filters = [POSKeepFilter(['名詞'])]
+a = Analyzer(char_filters=[], tokenizer=t, token_filters=token_filters)
+for token in a.analyze(reg_txt):
+    print(token)
+
+f.close()
